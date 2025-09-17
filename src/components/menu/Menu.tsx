e246d5e86a9cb3d8ce808854177cd6f1f58934f6
jjosephd <jjosephd804@gmail.com>
2025-09-16 20:48:07 -0400

    back button added to menu page

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,12 +1,16 @@
-
-
 import { products } from '../../data/productData';
+import { useNavigate } from 'react-router-dom';
 const Menu = () => {
   return (
-    <div className="text-white">
-      {products.map((item) => {
-        return <div key={item.id}>{item.name}</div>;
-      })}
+    <div className="m-4">
+      <div className="text-white">
+        {products.map((item) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
+      <button className="btn btn-warning" onClick={() => useNavigate()(-1)}>
+        Previous Page
+      </button>
     </div>
   );
 };
