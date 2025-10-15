daeb808d5e2adafb06d46fdc869d1b4223b18419
jjosephd <jjosephd804@gmail.com>
2025-10-15 15:02:01 -0400

    review ui update

--- src/components/user/ReviewCard.tsx
+++ src/components/user/ReviewCard.tsx
@@ -7,9 +7,9 @@ interface ReviewCardProps {
 const ReviewCard = ({ review }: ReviewCardProps) => {
   return (
     <div className="review-card border p-4 bg-slate-600/50 border-white rounded-lg mb-4 shadow-lg font-sans text-sm">
-      <h3 className="font-bold text-lg">{review.name}</h3>
+      <h3 className="font-bold text-lg text-white">{review.name}</h3>
       <p className="text-gray-600 text-xs">{review.email}</p>
-      <p className="text-gray-800 mt-2">{review.review}</p>
+      <p className=" mt-2 text-white">{review.review}</p>
     </div>
   )
 }
