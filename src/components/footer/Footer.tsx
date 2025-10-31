8cc1d68ae0f331395a34ca40d6cce5e916274b95
jjosephd <jjosephd804@gmail.com>
2025-10-31 11:42:30 -0400

    Footer fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,31 @@
+import { navLinks, subLinks } from '../../data/navLinks';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className="bg-orange-500 w-full h-auto sm:h-48">
-      
+      <ul className="flex flex-col sm:flex-row sm:mx-auto justify-between sm:gap-6 p-4">
+        {navLinks.map((link, index) => (
+          <li key={index} className="">
+            <NavLink to={link.to}>
+              <span className="uppercase text-white text-2xl sm:text-4xl font-extrabold hover:cursor-pointer">
+                {link.name}
+              </span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex flex-col sm:flex-row sm:mx-auto justify-between gap-6 p-4">
+        {subLinks.map((link, index) => (
+          <li key={index} className="">
+            <NavLink to={'#'}>
+              <span className="text-white font-extrabold hover:cursor-pointer">
+                {link.name}
+              </span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
