55ea3870bd40a66c9c85df13a9eb3dba1cb38b1a
jjosephd <jjosephd804@gmail.com>
2025-11-02 20:09:14 -0500

    grid fixing for customer favorites

--- src/components/home/favorites/CustomerFavorites.tsx
+++ src/components/home/favorites/CustomerFavorites.tsx
@@ -7,7 +7,7 @@ const CustomerFavorites = () => {
       <h2 className="text-3xl font-bold text-center mb-10 text-white">
         Customer Favorites
       </h2>
-      <div className="grid grid-cols-2 gap-4 w-full max-w-4xl mx-auto aspect-square">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-4xl mx-auto">
         {customerFavorites.map((item) => (
           <CustomerFavoritesCard key={item.id} item={item} />
         ))}
