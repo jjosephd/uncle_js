60435e59138cdf48123ce81dc1cba2c8ddab99b4
jjosephd <jjosephd804@gmail.com>
2025-12-11 09:34:07 -0500

    playwright integrated

--- src/data/customerFavorites.ts
+++ src/data/customerFavorites.ts
@@ -4,7 +4,8 @@ import nightlifeImg from '../assets/images/customer-favorites/nightlifeImg.jpg';
 import partyImg from '../assets/images/customer-favorites/partyImg.jpg';
 import type { Product } from './productData';
 
-export interface CustomerFavorite extends Omit<Product, 'category' | 'nutrition'> {
+export interface CustomerFavorite
+  extends Omit<Product, 'category' | 'nutrition'> {
   tags: string[];
 }
 
@@ -13,33 +14,36 @@ export const customerFavorites: CustomerFavorite[] = [
     id: 1,
     name: 'Margarita Mondays',
     imageUrl: margaritaImg,
-    description: 'Bring a friend with you on Mondays and enjoy a $2.50 margarita!',
-    price: 2.50,
-    tags: ['21+']
+    description:
+      'Bring a friend with you on Mondays and enjoy a $2.50 margarita!',
+    price: 2.5,
+    tags: ['21+'],
   },
   {
     id: 2,
     name: 'Saturday Smash',
     imageUrl: nightlifeImg,
-    description: 'Saturdays just for you! Join us for live music and half off drinks from 6pm to 10pm!',
+    description:
+      'Saturdays just for you! Join us for live music and half off drinks from 6pm to 10pm!',
     price: 14.99,
-    tags: ['Tickets Required']
+    tags: ['Tickets Required'],
   },
   {
     id: 3,
     name: 'After Hours Casino',
     imageUrl: partyImg,
-    description: 'Make your after hours experience unforgettable with our casino-style games and live music!',
+    description:
+      'Make your after hours experience unforgettable with our casino-style games and live music!',
     price: 12.99,
-    tags: ['18+', 'Tickets Required']
+    tags: ['18+', 'Tickets Required'],
   },
   {
     id: 4,
     name: 'Food Truck Fridays',
     imageUrl: foodTruckImg,
-    description: 'Catch us at the uptown nightlife area on Fridays when we take our show on the road.',
+    description:
+      'Catch us at the uptown nightlife area on Fridays when we take our show on the road.',
     price: 0,
-    tags: ['Free']
+    tags: ['Free'],
   },
- 
 ];
